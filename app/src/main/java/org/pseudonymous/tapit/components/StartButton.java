4854335f02e5adeb2eb1e0f0c1095645dd35b015
Eli Smith <plunkinguitar@gmail.com>
2017-09-14 13:41:42 -0500

    Added method to startbutton end

--- app/src/main/java/org/pseudonymous/tapit/components/StartButton.java
+++ app/src/main/java/org/pseudonymous/tapit/components/StartButton.java
@@ -3,37 +3,46 @@ package org.pseudonymous.tapit.components;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.animation.LinearInterpolator;
 
-/**
- *
- * Created by smerkous on 9/12/17.
- */
-
 public class StartButton extends android.support.v7.widget.AppCompatButton {
+    public int activityWidth;
+    public int activityHeight;
     public StartButton(Context context) {
         super(context);
     }
+
     public StartButton(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+
     public StartButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    public void startAnimation() {
-        final ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, -300f);
-        final ValueAnimator downAnimator = ValueAnimator.ofFloat(-300f, 600f);
+    public void inherit(Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay().getSize(size);
+        this.activityHeight = size.y;
+        this.activityWidth = size.x;
+    }
 
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                float value = (float) valueAnimator.getAnimatedValue();
-                setTranslationY(value);
-            }
-        });
+    public void startAnimation() {
+        final int buttonWidth = this.getWidth();
+        final int buttonHeight = this.getHeight();
+        final ValueAnimator downAnimator = ValueAnimator.ofFloat(0, buttonHeight);
+
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+//                float value = (float) valueAnimator.getAnimatedValue();
+//                setTranslationY(value);
+//            }
+//        });
 
         downAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
@@ -42,21 +51,16 @@ public class StartButton extends android.support.v7.widget.AppCompatButton {
                 setTranslationY(value);
             }
         });
-
-        valueAnimator.addListener(new AnimatorListenerAdapter() {
+        downAnimator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
-                downAnimator.start();
+                // This is where we will start GameEngine
             }
         });
 
-        valueAnimator.setInterpolator(new LinearInterpolator());
-        valueAnimator.setDuration(300);
-
+        downAnimator.start();
         downAnimator.setInterpolator(new LinearInterpolator());
-        valueAnimator.setDuration(1500);
-
-        valueAnimator.start();
     }
+
 }
